372128d83ad35c5abee453e15c4a578e775a3b4a
sattam.ghosal@alefedge.com <sattam.ghosal@alefedge.com>
2023-05-27 23:15:29 +0530

    minor documentation change

--- router/router.js
+++ router/router.js
@@ -2,7 +2,7 @@ let express =require("express");
 const Router = express.Router();
 let controller= require( "../controllers/UrlShortener.js");
 Router.get("", function (req, res) {
-    controller.fetchAllUrls(req, res, req.query["url_id"]);
+    controller.fetchAllUrls(req, res, req.query["shortened_url"]);
 });
 Router.post("", function (req, res) {
     controller.shortenUrl(req, res, req.body);
